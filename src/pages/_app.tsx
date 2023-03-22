473f11c2e9799e5251bc5c2e168c0d0b72654b59
thegitparticle <maddamsettysanjit@gmail.com>
2023-03-22 23:01:11 +0530

    add: next.js setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Albert_Sans } from "next/font/google";
+import Head from "next/head";
+
+const albertSans = Albert_Sans({
+	variable: "--font-albert-sans",
+	display: "swap",
+	subsets: ["latin"],
+});
+
+function HeadMetaData() {
+	return (
+		<>
+			<title>SpillTea</title>
+			<meta name="description" content="share your web3 stack" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1"
+			/>
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>{`
+				:root {
+					--font-albert-sans: ${albertSans.style.fontFamily};
+				}
+			`}</style>
+			<HeadMetaData />
+			<Component {...pageProps} />
+		</>
+	);
 }
