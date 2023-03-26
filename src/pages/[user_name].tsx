0571d8fd0f5e038a619693f9a8972fddcb36930c
thegitparticle <maddamsettysanjit@gmail.com>
2023-03-26 18:17:39 +0530

    enhance: stack page entirely server renders

--- src/pages/[user_name].tsx
+++ src/pages/[user_name].tsx
@@ -1,45 +1,51 @@
 /* eslint-disable @next/next/no-img-element */
-import PublishedStackList from "@/components/published/PublishedStackList";
 import { withSessionSsr } from "@/config/withsession";
 import { chainsList } from "@/info/chains";
 import { getDappDetails } from "@/network/getDappDetails";
 import { getUserDetailsbyUsername } from "@/network/getUserDetailsbyUsername";
+import { Dapp } from "@/types/dapp";
 import { User } from "@/types/user";
 import { useQuery } from "@tanstack/react-query";
 import { GetServerSideProps } from "next";
 
-export default function Stack({ userDetails }: { userDetails: User }) {
-	function DappThumbnail({ dappId }: { dappId: string }) {
-		const { isLoading, isError, data, error } = useQuery({
-			queryKey: ["dappDetails", dappId],
-			queryFn: getDappDetails,
-		});
+export default function Stack({
+	userDetails,
+	myDapps,
+}: {
+	userDetails: User;
+	myDapps: Dapp[];
+}) {
+	function DappThumbnail({ dapp }: { dapp: Dapp }) {
+		// const { isLoading, isError, data, error } = useQuery({
+		// 	queryKey: ["dappDetails", dappId],
+		// 	queryFn: getDappDetails,
+		// });
 
-		if (isLoading) {
-			return <span>Loading...</span>;
-		}
+		// if (isLoading) {
+		// 	return <span>Loading...</span>;
+		// }
 
-		if (isError) {
-			return <span>Error ...</span>;
-		}
+		// if (isError) {
+		// 	return <span>Error ...</span>;
+		// }
 
 		return (
-			<div className="card card-side shadow-xl bg-base-100">
+			<div className="card card-side shadow-xl bg-base-100 hover:scale-110">
 				<figure className="flex-col p-2">
 					<img
-						src={data[0].images?.logo}
+						src={dapp.images?.logo}
 						alt="dapp-logo"
 						className="rounded-full max-h-56 p-2"
 					/>
 				</figure>
 				<div className="card-body">
-					<p className="card-title text-base -mx-4">{data[0].name}</p>
+					<p className="card-title text-base -mx-4">{dapp.name}</p>
 					<p className="text-xs -mx-4">
-						{data[0].description.substring(0, 100)}
+						{dapp.description.substring(0, 100)}
 					</p>
 					<div className="flex card-actions justify-end flex-row">
-						{data[0].chains
-							? data[0].chains.map((chainId, key) => {
+						{dapp.chains
+							? dapp.chains.map((chainId, key) => {
 									return (
 										<div
 											key={key}
@@ -69,8 +75,8 @@ export default function Stack({ userDetails }: { userDetails: User }) {
 					{userDetails.user_name}&apos;s web3 stack
 				</div>
 				<div className="grid grid-cols-2 gap-6 mx-4 pb-8">
-					{userDetails.top_ten_dapps?.map((dappId, key) => (
-						<DappThumbnail key={key} dappId={dappId} />
+					{myDapps.map((dapp, key) => (
+						<DappThumbnail key={key} dapp={dapp} />
 					))}
 				</div>
 			</div>
@@ -86,8 +92,31 @@ export const getServerSideProps: GetServerSideProps = withSessionSsr(
 
 		const userDetails = await getUserDetailsbyUsername(user_name as string);
 
+		let myDapps: Dapp[] = [];
+
+		if (userDetails.top_ten_dapps !== null) {
+			for (const item of userDetails.top_ten_dapps) {
+				const options = {
+					method: "GET",
+					headers: { "Content-Type": "application/json" },
+				};
+
+				const dappsDetails = await fetch(
+					`https://api-a.meroku.store/dapp/searchById?dappId=${item}`,
+					options
+				);
+
+				const res = await dappsDetails.json();
+
+				myDapps.push(await res[0]);
+			}
+		}
+
 		return {
-			props: { userDetails: userDetails },
+			props: {
+				userDetails: JSON.parse(JSON.stringify(userDetails)),
+				myDapps,
+			},
 		};
 	}
 );
